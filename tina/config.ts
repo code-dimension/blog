fdd3d36cf5ba5e1da700ed04d0faab1af102a5fe
Henrique Custódia <martinshenrique8@gmail.com>
2023-10-27 17:44:58 -0300

    build: consome tokens do Tina via variáveis de ambiente

--- tina/config.ts
+++ tina/config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'tinacms'
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
+const clientId = process.env.TINA_CLIENT_ID || ''
+const token = process.env.TINA_TOKEN || ''
 
 export default defineConfig({
 	branch,
-	clientId: null, // Get this from tina.io
-	token: null, // Get this from tina.io
+	clientId,
+	token,
 
 	build: {
 		outputFolder: 'admin',
