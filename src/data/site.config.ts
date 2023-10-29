3c5e36ecd3346b5059f76c7ed81184c45279898d
Henrique Custódia <martinshenrique8@gmail.com>
2023-10-29 14:22:22 -0300

    chore: muitas alterações XO

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -8,10 +8,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG',
-	title: 'Astro Theme OpenBlog',
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post' // Message to share a post on social media
+	author: 'Code Dimension',
+	title: 'Blog da Code Dimension',
+	description:
+		'No Blog da Code Dimension você vai ter acesso as últimas novidades sobre Angular, boas práticas e Frontend em geral!',
+	lang: 'pt-BR',
+	ogLocale: 'pt_BR',
+	shareMessage: 'Compatilhe esse post!' // Message to share a post on social media
 }
--- src/utils/readTime.ts
+++ src/utils/readTime.ts
@@ -10,6 +10,20 @@ export function remarkReadingTime() {
 		const readingTime = getReadingTime(textOnPage)
 		// readingTime.text will give us minutes read as a friendly string,
 		// i.e. "3 min read"
-		data.astro.frontmatter.minutesRead = readingTime.text
+		data.astro.frontmatter.minutesRead = createMinutesReadText(readingTime.minutes)
+	}
+}
+
+function createMinutesReadText(minutes: number) {
+	let roundedMinutes = Math.round(minutes)
+
+	if (roundedMinutes < 1) {
+		roundedMinutes = 1
+	}
+
+	if (roundedMinutes > 1) {
+		return `${roundedMinutes} minutos de leitura`
+	} else {
+		return `1 minuto de leitura`
 	}
 }
